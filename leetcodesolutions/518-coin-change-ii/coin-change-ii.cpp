1ab3868c4cde9c76bbe002b9e1b3e82edc843e04
Viljen789 <90694640+Viljen789@users.noreply.github.com>
2024-10-14 11:09:31 +0200

    Time: 9 ms (66.99%) | Memory: 9.1 MB (84.73%) - LeetSync

--- leetcodesolutions/518-coin-change-ii/coin-change-ii.cpp
+++ leetcodesolutions/518-coin-change-ii/coin-change-ii.cpp
@@ -3,21 +3,20 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<ll>>dp(coins.size(), vector<ll>(amount+1, 0));
+        vector<ll> dp(amount+1, 0);
         sort(coins.begin(), coins.end());
         int i = 0;
-        while(i*coins[0]<=amount)dp[0][i++*coins[0]] = 1;
+        while(i*coins[0]<=amount)dp[i++*coins[0]] = 1;
         i = 0; 
         for(int i = 1; i<coins.size(); i++){
             for(int j = 0; j<=amount; j++){
-                if(dp[i][j]>=2e9)return 0;
-                dp[i][j] = + dp[i-1][j];
+                if(dp[j]>=2e9)return 0;
                 if(j>=coins[i]){
-                    if(dp[i][j] + dp[i][j-coins[i]]>=2e10)return 0;
-                    dp[i][j]+=dp[i][j-coins[i]];
+                    if(dp[j] + dp[j-coins[i]]>=2e10)return 0;
+                    dp[j]+=dp[j-coins[i]];
                 }
             }
         } 
-        return dp[n-1][amount];
+        return dp[amount];
     }
 };
