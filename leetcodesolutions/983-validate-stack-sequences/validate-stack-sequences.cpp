778a63dcac45cd8fc4d0a1ec2597bb0a1473d9c8
Viljen789 <90694640+Viljen789@users.noreply.github.com>
2024-11-18 21:46:07 +0100

    Time: 0 ms (100.00%) | Memory: 18.2 MB (31.19%) - LeetSync

---
+++ leetcodesolutions/983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = popped.size(); 
+        stack<int> s;
+        int j = 0;
+        for(int i = 0; i<n; i++){
+            s.push(pushed[i]);
+            while(!s.empty()&&s.top()==popped[j]){
+                s.pop();
+                j++;
+            }
+        }
+        return s.empty();
+    }
+};
